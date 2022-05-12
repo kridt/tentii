f0322f8d20e37a23ce1af4f8eacddf3cde8d07d9
Christian <chrnielsen2003@gmail.com>
2022-05-12 04:00:05 +0200

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Landingpage from "./pages/Landingpage";
 import "./App.scss";
 import { Router } from "@reach/router";
 import Sellers from "./pages/Sellers";
-import Discover from "./pages/Discover";
+/* import Discover from "./pages/Discover"; */
 import About from "./pages/About";
 import ProductPage from "./pages/ProductPage";
 import SellerSIte from "./pages/SellerSIte";
@@ -14,12 +14,12 @@ function App() {
     <Router className="App">
       <Landingpage exact path="/" />
       <Sellers path="/sellers" />
-      <Discover path="/discover" />
+      {/* <Discover path="/discover" /> */}
       <About path="/about" />
       <ProductPage path="/product/:id" />
       <SellerSIte path="/sellerSite/:id" />
       <Signup path="/signup" />
-      <SignUpSeller  path="/signUpSeller" />
+      <SignUpSeller path="/signUpSeller" />
     </Router>
   );
 }
