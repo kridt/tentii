08e45e9549b5027654767347a5dd8d3fce0ea2a9
Christian <chrnielsen2003@gmail.com>
2022-05-14 23:31:00 +0200

    .

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,30 +1,26 @@
-import React from 'react';
-import NavBar from '../components/NavBar';
-import SideNav from '../components/SideNav';
+import React from "react";
+import NavBar from "../components/NavBar";
+import SideNav from "../components/SideNav";
 import "./Signup.scss";
-import { Link } from '@reach/router';
+import { Link } from "@reach/router";
 
 export default function Signup() {
-
-
   return (
-    <div className='signUpPage'>  
-        <NavBar />
-        <SideNav />
-        <div style={{textAlign: "center"}}>
-
+    <div className="signUpPage">
+      <NavBar />
+      <SideNav />
+      <div style={{ textAlign: "center" }}>
         <h1>Opret din profil</h1>
         <h2>Hvor starter rejsen?</h2>
+      </div>
+      <div className="signUpCoise">
+        <div className="signUpCoise__left">
+          <Link to="/">Kunde</Link>
         </div>
-        <div className='signUpCoise'>
-            <div className='signUpCoise__left'>
-                <Link to='/'>Kunde</Link>
-            </div>
-            <div className='signUpCoise__right'>
-                <Link to='/signUpSeller/stepOne'>Sælger</Link>
-            </div>
+        <div className="signUpCoise__right">
+          <Link to="/stepper">Sælger</Link>
         </div>
-
+      </div>
     </div>
-  )
+  );
 }
